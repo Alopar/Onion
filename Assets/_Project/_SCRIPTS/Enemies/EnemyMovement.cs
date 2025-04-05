7469f684694b84a6369322b6e7bb275ad8b224dd
Alex Lopar <alopar86@gmail.com>
2025-04-05 23:52:46 +0300

    enemies spawn at circle border

--- Assets/_Project/_SCRIPTS/Enemies/EnemyMovement.cs
+++ Assets/_Project/_SCRIPTS/Enemies/EnemyMovement.cs
@@ -21,14 +21,23 @@ namespace Gameplay
         private void Update()
         {
             if (_core == null) return;
-            Move(_core.position - transform.position);
+
+            var direction = _core.position - transform.position;
+            Move(direction);
+            Rotate(direction);
         }
         #endregion
 
         #region METHODS PRIVATE
         private void Move(Vector3 direction)
         {
-            transform.Translate(direction.normalized * (_speed * Time.deltaTime));
+            transform.position += direction.normalized * (_speed * Time.deltaTime);
+        }
+
+        private void Rotate(Vector3 direction)
+        {
+            var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, angle);
         }
         #endregion
     }
